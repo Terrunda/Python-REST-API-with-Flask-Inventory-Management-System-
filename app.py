c464655ec89d8d0aedc1b447f59d2ec8cc6b2399
Terrunda <terrencegift49@gmail.com>
2026-04-08 07:44:33 +0300

    Removed unnecessary comments on app.py whilst adding functionality to some of the CRUD operations.

--- app.py
+++ app.py
@@ -5,41 +5,41 @@ app = Flask(__name__)
 
 # Temporary array to simulate storage. The storage could contain data from the OPenFoodFacts API.
 inventory = []
-
-# Mock database. Each item must contain an id.
-
-# Function to query OpenFoodFacts API either using 'barcode' or 'product name'
-
-
-
+next_inventory_id = 0 # Key ffor identifying unique inventory based on id.
 
 
 #Flask Routes and their view functions
-@app.route('/inventory', methods=['GET']) #Route for fetching items(products) in the inventory.
+@app.route('/inventory', methods=['GET'])
 def view_inventory():
-    # return jsonify(inventory), 200
-    return 'This URL will be used to fetch all items in the inventory.' 
+    return jsonify(inventory), 200
+
 
 @app.route('/inventory/<int:id>', methods=['GET'])
 def view_inventory_item(id):
     inventory_query = next((item for item in inventory if item["id"] == id), None)
-    if not inventory_query:
-        return f'Item with id: {id} not found', 404
-    elif inventory_query:
+    if inventory_query == None:
+        return jsonify(f'Item with id: {id} not found'), 404
+    else:
         return jsonify(inventory_query), 200
-    return f'This URL will fetch a single item with the id: {id}'
 
 @app.route('/inventory', methods=['POST'])
 def add_inventory_item():
-    return 'This URL will be used to add a new item to the inventory.'
+    request_data = request.get_json()
 
 @app.route('/inventory/<int:id>', methods= ['PATCH'])
 def update_inventory_item(id):
+    request_data = request.get_json()
     return f'This URL will update an inventory with the id: {id}'
 
 @app.route('/inventory/<int:id>', methods=['DELETE'])
 def delete_inventory_item(id):
-    return f'This URL will be used to delete an item from the inventory with the id: {id}'
+    global inventory
+    initial_len = len(inventory)
+    inventory = [i for i in inventory if i['id'] != id]
+    
+    if len(inventory) < initial_len:
+        return jsonify("Item deleted"), 204
+    return jsonify("Error: Item not found"), 404
 
 
 
