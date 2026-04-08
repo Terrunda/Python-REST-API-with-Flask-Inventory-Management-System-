0848e31694f427f98e6433be4186b4b2dea3bc65
Terrunda <terrencegift49@gmail.com>
2026-04-08 08:49:04 +0300

    Implemented functionality for  te PATCH request in Flask, added a new file index.html to act as the front end.

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ inventory = []
 next_inventory_id = 0 # Key ffor identifying unique inventory based on id.
 
 
+# structure = {
+#   "status": 1,
+#   "product": {
+#     "product_name": "Organic Almond Milk",
+#     "brands": "Silk",
+#     "ingredients_text": "Filtered water, almonds, cane sugar, ..."
+#     // Additional product information
+#   }
+
+# OpenFoodFacts URLS.
+# - Get product by barcode: https://world.openfoodfacts.net/api/v2/product/3017624010701?fields=product_name,nutriscore_data or https://world.openfoodfacts.net/api/v2/product/{barcode}
+
+# function should be defined for searching API.
+
 #Flask Routes and their view functions
 @app.route('/inventory', methods=['GET'])
 def view_inventory():
@@ -24,12 +38,39 @@ def view_inventory_item(id):
 
 @app.route('/inventory', methods=['POST'])
 def add_inventory_item():
-    request_data = request.get_json()
+    request_data = request.get_json() # Retrieve user input.
+    global next_inventory_id
+    
+    if not request_data or 'name' not in request_data:
+        return jsonify({"error": "Item name is required"}), 400
+
+    new_item = {
+        "id": next_inventory_id,
+        "name": request_data['name'],
+        "price": request_data.get('price', 0.0),
+        "stock": request_data.get('stock', 0),
+        "barcode": request_data.get('barcode', None)
+    }
+
+    inventory.append(new_item)
+    next_inventory_id += 1
+    return jsonify(new_item), 201
 
 @app.route('/inventory/<int:id>', methods= ['PATCH'])
 def update_inventory_item(id):
+    item = next((i for i in inventory if i['id'] == id), None)
+    if not item:
+        return jsonify({"Error": "Item not found"}), 404
+
     request_data = request.get_json()
-    return f'This URL will update an inventory with the id: {id}'
+    if 'price' in request_data:
+        item['price'] = request_data['price']
+    if 'stock' in request_data:
+        item['stock'] = request_data['stock']
+    if 'name' in request_data:
+        item['name'] = request_data['name']
+
+    return jsonify(item), 200
 
 @app.route('/inventory/<int:id>', methods=['DELETE'])
 def delete_inventory_item(id):
@@ -40,8 +81,6 @@ def delete_inventory_item(id):
     if len(inventory) < initial_len:
         return jsonify("Item deleted"), 204
     return jsonify("Error: Item not found"), 404
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- front_end/index.js
+++
@@ -1,8 +0,0 @@
-const OPENFOODFACTS_API_ENDPOINT = 'https://world.openfoodfacts.net'
-
-fetch(`${OPENFOODFACTS_API_ENDPOINT}/api/v2/product/3274080005003.json`, {
-  method: "GET",
-  headers: { Authorization: "Basic " + btoa("off:off") },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
